831c9f455f331694a2e77fb21ee5f55cb65f463e
Kirill7758 <kirillg7758@gmail.com>
2020-09-11 19:50:53 +0300

    remove dependencive Search Hook

--- src/client/Main/pages/AddCityPage/Search/Search.jsx
+++ src/client/Main/pages/AddCityPage/Search/Search.jsx
@@ -28,7 +28,7 @@ const Search = (props) => {
                 setCountryName(newArr)
             })
         setListCountryShow(false)
-    }, [])
+    })
 
     const onChangeInput = (item) => {
         setValue(item)
